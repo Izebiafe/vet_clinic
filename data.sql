a95e07dee3b04f25c6cd40238a232d3dbfe192eb
izebiafe <afimiafidelis95@gmail.com>
2023-10-24 00:08:47 +0100

    add all requirents

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES (1, 'Agumon','Feb 3rd, 2020', 0, true, 10.23),
+(2, 'Gabumon','Nov 15th, 2018', 2, true, 8),
+(3, 'Pikachu','Jan 7th, 2021', 1, false, 15.04),
+(4, 'Devimon','May 12th, 2017', 5, true, 11);
---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE animals (
+
+    id: integer
+    name: string
+    date_of_birth: date
+    escape_attempts: integer
+    neutered: boolean
+    weight_kg: decimal
+
+);
