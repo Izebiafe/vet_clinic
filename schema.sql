375224f75f04be4d3c5128e4bdf6a0aebe8227b9
izebiafe <afimiafidelis95@gmail.com>
2023-10-28 08:09:35 +0100

    add schema to change id to auto increment

--- schema.sql
+++ schema.sql
@@ -23,3 +23,33 @@ CREATE TABLE owners (
   full_name TEXT,
   age INTEGER
 );
+
+-- Create a table named species with the following columns:
+
+--     id: integer (set it as autoincremented PRIMARY KEY)
+--     name: string
+
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+-- Modify animals table:
+
+--     Make sure that id is set as autoincremented PRIMARY KEY
+--     Remove column species
+--     Add column species_id which is a foreign key referencing species table
+--     Add column owner_id which is a foreign key referencing the owners table
+
+
+ALTER TABLE animals ADD COLUMN new_id serial;
+
+UPDATE animals SET new_id = id;
+
+ALTER TABLE animals DROP CONSTRAINT animals_pkey;
+ALTER TABLE animals DROP COLUMN id;
+
+ALTER TABLE animals RENAME COLUMN new_id TO id;
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
