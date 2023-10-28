9e5d1af275266b76033f8c74322f0368ce743bb8
izebiafe <afimiafidelis95@gmail.com>
2023-10-28 07:18:54 +0100

    Delete and update weight using savepoint

--- queries.sql
+++ queries.sql
@@ -4,20 +4,41 @@ SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
 
 SELECT * FROM animals;
 
 BEGIN TRANSACTION;
 
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-
-UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+DELETE FROM animals;
 
 SELECT * FROM animals;
 
-COMMIT;
+ROLLBACK;
 
 SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT savepoint_1;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT savepoint_1;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT; 
