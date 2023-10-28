c706002550da4557420ead6bca2a678232cdfe5c
izebiafe <afimiafidelis95@gmail.com>
2023-10-28 08:15:31 +0100

    instert into owners table

--- data.sql
+++ data.sql
@@ -5,12 +5,21 @@ VALUES (1, 'Agumon','2020/03/03', 0, true, 10.23),
 (4, 'Devimon','2017/05/12', 5, true, 11);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES-- Insert the following data into the owners table:
+
+--     Sam Smith 34 years old.
+--     Jennifer Orwell 19 years old.
+--     Bob 45 years old.
+--     Melody Pond 77 years old.
+--     Dean Winchester 14 years old.
+--     Jodie Whittaker 38 years old.
+
+INSERT INTO owners (full_name, age)
 VALUES
-    (5, 'Charmander', '2020-02-08', 0, false, -11),
-    (6, 'Plantmon', '2021-11-15', 2, true, -5.7),
-    (7, 'Squirtle', '1993-04-02', 3, false, -12.13),
-    (8, 'Angemon', '2005-06-12', 1, true, -45),
-    (9, 'Boarmon', '2005-06-07', 7, true, 20.4),
-    (10, 'Blossom', '1998-10-13', 3, true, 17),
-    (11, 'Ditto', '2022-05-14', 4, true, 22);
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
 
