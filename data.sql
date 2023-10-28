50c6a1f422a0e3203cbf2eb958be16661280ce9c
izebiafe <afimiafidelis95@gmail.com>
2023-10-28 09:34:29 +0100

    Vet Stephanie Mendez is specialized in Digimon and Pokemon

--- data.sql
+++ data.sql
@@ -80,3 +80,7 @@ VALUES
     --  Vet William Tatcher is specialized in Pokemon.
     INSERT INTO specializations (vet_id, species_id)
     VALUES ((SELECT id FROM vets WHERE name = 'William Tatcher'), 1);
+
+   --  Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+    INSERT INTO specializations (vet_id, species_id)
+    VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), 2);
