ef125669d87b1752b2e7cfcdf8a8804c9fa2afc5
izebiafe <afimiafidelis95@gmail.com>
2023-10-28 08:04:02 +0100

    Add multiple tables shema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,39 @@ CREATE TABLE animals(
   date_of_birth date,
   escape_attempts integer,
   neutered boolean,
-  weight_kg decimal
+  weight_kg decimal,
+  species text
 );
 
 ALTER TABLE animals ADD species text;
+
+-- Create a table named owners with the following columns:
+
+--     id: integer (set it as autoincremented PRIMARY KEY)
+--     full_name: string
+--     age: integer
+
+
+CREATE TABLE owners(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  full_name text,
+  age integer
+);
+
+-- Create a table named species with the following columns:
+
+--     id: integer (set it as autoincremented PRIMARY KEY)
+--     name: string
+
+
+CREATE TABLE species(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  name text
+);
+
+-- Modify animals table:
+
+--     Make sure that id is set as autoincremented PRIMARY KEY
+--     Remove column species
+--     Add column species_id which is a foreign key referencing species table
+--     Add column owner_id which is a foreign key referencing the owners table
