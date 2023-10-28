797b400a76a7a2062bb0d06d642e7ad51c87f687
izebiafe <afimiafidelis95@gmail.com>
2023-10-28 07:15:10 +0100

    Set new species name based on names and null

--- queries.sql
+++ queries.sql
@@ -6,3 +6,18 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+SELECT * FROM animals;
+
+COMMIT;
+
+SELECT * FROM animals;
