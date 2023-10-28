6fda4bdb7df42664ce447b5dd088c2aa67fcdd30
izebiafe <afimiafidelis95@gmail.com>
2023-10-28 07:10:08 +0100

    Add query to add species to animal table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,11 @@
-CREATE TABLE animals (
-    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    name VARCHAR(255),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL(5, 2)
+
+CREATE TABLE animals(
+  id integer,
+  name text,
+  date_of_birth date,
+  escape_attempts integer,
+  neutered boolean,
+  weight_kg decimal
 );
+
+ALTER TABLE animals ADD species text;
